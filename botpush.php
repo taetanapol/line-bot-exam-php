efd7651e1d05e61800738c6b06ba31b9137dceb5
taetanapol <31063705+taetanapol@users.noreply.github.com>
2018-10-28 03:18:31 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'mAq8004CMX48UWBUngW6Z2pJ2Jjl+D0AWBe3sw6JNK+qUzfAM7h/nmSYteJrFV3F2FrHsPRzuUgNCanxatVU2EGckhdGilQpOE+1Zk2MaY52shPDMfbb6VyyU7SDlWFvfsd2dLmTn/GAgt06Ax72NwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'f74fa11e6fa039b659e94b667cf290a4';
+$channelSecret = 'ed31709ed1aa7ab883292e0c081fafe4';
 
 $pushID = 'U49431d7079eecf6ee3e7181691d36357';
 
